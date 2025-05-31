f16d3c4530fb4913c4cc03fc7603c9dcd8a17f9d
Jamie Evawin <jamie.evawin@codecomputerlove.com>
2025-05-31 17:59:16 +0100

    style: 💄 Allow changing of size and weight in text links

--- app/components/01-atoms/text-link/text-link.tsx
+++ app/components/01-atoms/text-link/text-link.tsx
@@ -2,20 +2,30 @@ import type { LinkProps } from 'react-router';
 
 import { Link } from 'react-router';
 import { clsx } from 'clsx';
-import { Text } from '../text/text';
+import { Text, type TTextSizes, type TTextWeights } from '../text/text';
 
 import styles from './text-link.module.css';
 
-export type TTextLink = { linkText: string } & LinkProps;
+export type TTextLink = {
+	linkText: string;
+	size?: TTextSizes;
+	weight?: TTextWeights;
+} & LinkProps;
 
-export const TextLink: React.FC<TTextLink> = ({ to, linkText, className }) => {
+export const TextLink: React.FC<TTextLink> = ({
+	to,
+	linkText,
+	size = '100',
+	weight = '200',
+	className,
+}) => {
 	return (
 		<Link
 			className={clsx(styles.link, className)}
 			data-e2e-id="text-link"
 			to={to}
 		>
-			<Text size="100" weight="200" role="presentation">
+			<Text size={size} weight={weight} role="presentation">
 				{linkText}
 			</Text>
 		</Link>
