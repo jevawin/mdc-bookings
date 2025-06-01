46dc7349c662dd94e026a9e0a8dc9045d181b9db
Jamie Evawin <jamie.evawin@codecomputerlove.com>
2025-06-01 19:50:21 +0100

    style: 💄 llow description, required, optional

--- app/components/02-molecules/form-inputs-group/form-inputs-group.tsx
+++ app/components/02-molecules/form-inputs-group/form-inputs-group.tsx
@@ -5,18 +5,34 @@ import { Text } from '~/components/01-atoms/text/text';
 export type TFormInputsGroup = {
 	items: TRadio[];
 	label?: string;
+	description?: string;
 	isRequired?: boolean;
 };
 
 export const FormInputsGroup: React.FC<TFormInputsGroup> = ({
 	items,
 	label,
+	description,
 	isRequired,
 }) => {
+	const reqOpt = isRequired ? '(Required)' : '(Optional)';
+
 	return (
 		<>
 			<div className={styles.base}>
-				{label ? <Text weight="200">{label}</Text> : null}
+				<div className={styles.textWrapper}>
+					{label ? (
+						<Text weight="200">
+							{label}{' '}
+							<Text weight="200" color="brand">
+								{reqOpt}
+							</Text>
+						</Text>
+					) : null}
+					{description ? (
+						<Text weight="100">{description}</Text>
+					) : null}
+				</div>
 				<fieldset className={styles.inputsContainer}>
 					{items.map((item) => {
 						return <Radio {...item} />;
