90ad921a0750ff638c84826d9876797c8e46a720
Alex Clapperton <hi@alexclapperton.co.uk>
2025-08-03 18:19:39 +0100

    chore: 🎨 Update auto complete attributes

--- app/components/05-templates/book-interpreter-template/book-interpreter-template.tsx
+++ app/components/05-templates/book-interpreter-template/book-interpreter-template.tsx
@@ -558,6 +558,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 							id="company-name"
 							label="What's your company's name?"
 							name="companyName"
+							autoComplete="organization"
 							isRequired={false}
 							showRequired={true}
 						/>
@@ -574,6 +575,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 									id: 'company-address-1',
 									label: 'Address line 1',
 									name: 'financeAddress1',
+									autoComplete: 'address-line1',
 									isInvalid: Boolean(
 										fieldErrors?.financeAddress1,
 									),
@@ -584,6 +586,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 									id: 'company-address-2',
 									label: 'Address line 2',
 									name: 'financeAddress2',
+									autoComplete: 'address-line2',
 									isRequired: false,
 									showRequired: true,
 								},
@@ -591,6 +594,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 									id: 'company-address-city',
 									label: 'Town or city',
 									name: 'financeCity',
+									autoComplete: 'address-level2',
 									isInvalid: Boolean(
 										fieldErrors?.financeCity,
 									),
@@ -601,6 +605,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 									id: 'company-address-postcode',
 									label: 'Postcode',
 									name: 'financePostcode',
+									autoComplete: 'postal-code',
 									isInvalid: Boolean(
 										fieldErrors?.financePostcode,
 									),
@@ -615,6 +620,7 @@ export const BookInterpreterTemplate: React.FC<TBookInterpreterTemplate> = ({
 							id="finance-email"
 							label="What's your finance email address?"
 							name="financeEmail"
+							autoComplete="work email"
 							hint="We'll send invoices to this email address."
 							isRequired={false}
 							showRequired={true}
